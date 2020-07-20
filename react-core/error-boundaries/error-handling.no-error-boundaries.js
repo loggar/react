6b6bc4e555efcdc8f9bffe6f01a03795bec30b63
Charly LEE <charly.loggar@gmail.com>
2020-07-20 15:31:29 +1000

    feat (error-boundaries)

--- react-core/error-boundaries/error-handling.no-error-boundaries.js
+++ react-core/error-boundaries/error-handling.no-error-boundaries.js
@@ -1,9 +1,7 @@
 /*
 Error boundaries do not catch errors inside event handlers.
 
-React doesn’t need error boundaries to recover from errors in event handlers.
-Unlike the render method and lifecycle methods, the event handlers don’t happen during rendering.
-So if they throw, React still knows what to display on the screen.
+React doesn’t need error boundaries to recover from errors in event handlers. Unlike the render method and lifecycle methods, the event handlers don’t happen during rendering. So if they throw, React still knows what to display on the screen.
 
 If you need to catch an error inside event handler, use the regular JavaScript try / catch statement:
 */
@@ -27,7 +25,7 @@ class MyComponent extends React.Component {
     if (this.state.error) {
       return <h1>Caught an error.</h1>;
     }
-    return <div onClick={this.handleClick}>Click Me</div>;
+    return <button onClick={this.handleClick}>Click Me</button>;
   }
 }
 
